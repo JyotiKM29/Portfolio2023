b1e2d9ecc94682382730e7e274c711774ff8975c
Jyoti KM <jyotikumari4442@gmail.com>
2023-04-27 14:47:58 +0530

    favicon 3

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-     <Head>
-          <link rel="icon" href="/jyoti.ico" />
-        </Head>
-    
+      <Head>
+        <title>Jyoti Km</title>
+        <link rel="icon" href="/jyoti.ico" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
