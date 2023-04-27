f6bdbe3d237391a759db84059902c72e1f6e5d53
Jyoti KM <jyotikumari4442@gmail.com>
2023-04-27 13:27:33 +0530

    favicon is add

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <title>Jyoti Km</title>
+       <link rel="icon" type="image/x-icon" href='/jyoti.ico' />
+      </Head>
       <body>
         <Main />
         <NextScript />
