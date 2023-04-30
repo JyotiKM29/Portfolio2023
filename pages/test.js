01dc7c648e7582110ee10ac2d319af4f72c557c9
Jyoti KM <jyotikumari4442@gmail.com>
2023-04-30 23:55:23 +0530

    testing navbar in mobile

--- pages/test.js
+++ pages/test.js
@@ -1,9 +1,25 @@
-import HamburgerMenu from "@/components/hamburgerMenu";
+import styles from "../styles/Component/nav.module.scss";
+
 
 function Test() {
     return (
       <>
-      <HamburgerMenu />
+      <div className={styles.navbar}>
+      <label className={styles.hamburgerMenu}>
+           <input type="checkbox" className={styles.input} />
+      </label>
+      <aside className={styles.sidebar}>
+      <nav>
+        <div>Home </div>
+        <div>about </div>
+        <div>Project</div>
+        <div>Contact</div>
+        <div>Resume</div>
+      </nav>
+
+      </aside>
+      
+      </div>
       </>
     );
 }
