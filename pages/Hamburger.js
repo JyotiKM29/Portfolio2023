2836d54931443e2d619789b2aa8183786bd6a040
Jyoti KM <jyotikumari4442@gmail.com>
2023-05-01 11:03:26 +0530

    Links Updated in hamburger

--- pages/Hamburger.js
+++ pages/Hamburger.js
@@ -1,55 +1,56 @@
 import styles from "../styles/Component/Hamburger.module.scss";
 
 import Link from "next/link";
-import Image  from "next/image";
-import { useRouter } from 'next/router';
+
+import { useRouter } from "next/router";
 
 function Hamburger() {
   const router = useRouter();
   const downloadResume = () => {
-    router.push('/Jyoti KM.pdf');
+    router.push("/Jyoti KM.pdf");
   };
 
-  
-    return (
-      <>
+  return (
+    <>
       <div className={styles.navbar}>
-      <label className={styles.hamburgerMenu}>
-           <input type="checkbox" className={styles.input} />
-      </label>
-      <aside className={styles.sidebar}>
-      <nav className={styles.nav}  onclick="showMessage()" onmouseover="showTooltip()" onmouseout="hideTooltip()">
-      <Link href="/" className={styles.link}>
-            <span className={styles.span}>01.</span>Home
-          </Link>
-          
-          <Link href="/about" className={styles.link}>
-            <span className={styles.span}>02.</span>About
-          </Link>
-          
-          <Link href="/project" className={styles.link}>
-            <span className={styles.span}>03.</span>Project
-            
-          </Link>
-          {/* <Link href="/github" className={styles.link}>
-            <span className={styles.span}>04.</span>Experience
-          </Link> */}
-          
-          <Link href="/contact" className={styles.link}>
-            <span className={styles.span}>04.</span>Contact
-            
-          </Link>
-          {/* button */}
-          <Link href="#" className={styles.link}>
-            <button onClick={downloadResume} className={styles.btn}>Resume</button>
-          </Link>
-          
-      </nav>
+      {/* <div className={styles.blur}></div> */}
+        <label className={styles.hamburgerMenu}>
+          <input type="checkbox" className={styles.input} />
+        </label>
+        
+        <aside className={styles.sidebar}>
+          <nav
+            className={styles.nav}
+            onclick="showMessage()"
+            onmouseover="showTooltip()"
+            onmouseout="hideTooltip()"
+          >
+            <Link href="/" className={styles.nav__link}>
+              <span className={styles.span}>01.</span>Home
+            </Link>
 
-      </aside>
-      
+            <Link href="/about" className={styles.nav__link}>
+              <span className={styles.span}>02.</span>About
+            </Link>
+
+            <Link href="/project" className={styles.nav__link}>
+              <span className={styles.span}>03.</span>Project
+            </Link>
+
+            <Link href="/contact" className={styles.nav__link}>
+              <span className={styles.span}>04.</span>Contact
+            </Link>
+            {/* button */}
+            <Link href="#" className={styles.nav__link}>
+              <button onClick={downloadResume} className={styles.btn}>
+                Resume
+              </button>
+            </Link>
+          </nav>
+        </aside>
+        
       </div>
-      </>
-    );
+    </>
+  );
 }
-export default Hamburger
+export default Hamburger;
