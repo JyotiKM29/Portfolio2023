0087a6940e1c816b2a9dfa1c704175a33814e1d8
Jyoti KM <jyotikumari4442@gmail.com>
2023-05-16 09:54:21 +0530

    Update resume by addding my phone also

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,12 @@ import '@/styles/globals.css';
 import '@/styles/styles.scss';
 import Navbar from './navbar';
 import Hamburger from './Hamburger';
-
+import { ThemeProvider, useTheme } from 'next-themes';
 // import Link from 'next/link';
 
 export default function App({ Component, pageProps }) {
   return (
+    <ThemeProvider defaultTheme="light">
     <div>
       <Navbar />
       <Hamburger />
@@ -14,5 +15,6 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </main>
     </div>
+    </ThemeProvider>
   )
 }
