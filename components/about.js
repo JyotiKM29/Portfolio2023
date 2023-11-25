ada9788670bd9501a21d8e0b3594299c280533cf
JyotiKM29 <jyotikumari4442@gmail.com>
2023-11-25 23:11:26 +0530

    about page is updated

--- components/about.js
+++ components/about.js
@@ -11,8 +11,7 @@ function About() {
           <span className={styles.title__count}>02.</span>
           <h2 className={styles.title__name}>About Me</h2>
           <div className={styles.title__line}>
-          <hr style={{ borderWidth: '.1px', borderColor: '#8892B0' }} />
-
+            <hr style={{ borderWidth: ".1px", borderColor: "#8892B0" }} />
           </div>
         </div>
 
@@ -21,56 +20,187 @@ function About() {
           {/* detail */}
           <div className={styles.content__detail}>
             <p className={styles.p}>
-              Hello ! , My name is Jyoti KM. I enjoy creating things that live
-              on the internet. My interest in web development started back in
-              2017 when I decided to try animate custom heading on webpage turns
-              out take computer Science subject in school to Software
-              Engineering Branch in my b.tech Degree.
+              Hello! I'm Jyoti KM, passionate about crafting internet-based
+              creations. My journey into web development began in 2017 when I
+              explored animating custom headings on webpages. This interest led
+              me from studying Computer Science in school to pursuing{" "}
+              <span style={{ color: "#7cf1e2" }}>Software Engineering</span> in
+              my <span style={{ color: "#7cf1e2" }}>B.tech degree.</span>
             </p>
 
             <p className={styles.p}>
-              I can help you create a website that not only looks great but also
-              <span style={{color:"#7cf1e2"}}> drives traffic and boosts conversions </span>. Let me put my skills to
-              work and help you take your brand to the next level with a
-              visually stunning and user-friendly website.
+              With over <span style={{ color: "#7cf1e2" }}> 2+ years </span>of
+              expertise in frontend and full-stack development, I bring the
+              skills needed to create a flawless product and achieve your
+              desired results seamlessly.
             </p>
 
             <p className={styles.p}>
-              I recently build <Link style={{color:"#7cf1e2"}} href='https://town-of-books.vercel.app/'>Town of Book </Link> website from scratch UI design to
-              code design to add functionality to store data to authetication.
-            </p>
-
-            <p className={styles.p}>
-              Here are a few technologies I’ve been working with recently:
+              Here are a few technologies I’ve been worked  recently:
             </p>
 
             <div className={styles.cols}>
               <div className={styles.cols__1}>
-                <div><Image alt="arow" className={styles.arrow} src="/arrow.svg" width={"15"} height={"15"}></Image>
-                <span>Html5</span></div>
-                <div><Image alt="arow" className={styles.arrow} src="/arrow.svg" width={"15"} height={"15"}></Image>
-                <span>CSS3</span></div>
-                <div><Image alt="arow" className={styles.arrow} src="/arrow.svg" width={"15"} height={"15"}></Image>
-                <span>Sass</span></div>
+              <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>React</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>Redux</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>Context Api</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>Router</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>React Query</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>GraphQL</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>Socket.io</span>
+                </div>
               </div>
               <div className={styles.cols__2}>
-              <div><Image alt="arow" className={styles.arrow} src="/arrow.svg" width={"15"} height={"15"}></Image>
-                <span>JavaScript</span></div>
-                <div><Image alt="arow" className={styles.arrow} src="/arrow.svg" width={"15"} height={"15"}></Image>
-                <span>React</span></div>
-                <div><Image alt="arow" className={styles.arrow} src="/arrow.svg" width={"15"} height={"15"}></Image>
-                <span>Next.js</span></div>
+              <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>JavaScript</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>Typscript</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>Next.js</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>Figma</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>GSAP</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>SCSS</span>
+                </div>
+                <div>
+                  <Image
+                    alt="arow"
+                    className={styles.arrow}
+                    src="/arrow.svg"
+                    width={"15"}
+                    height={"15"}
+                  ></Image>
+                  <span>Tailwind</span>
+                </div>
+               
               </div>
             </div>
           </div>
 
           {/* photo */}
           <div className={styles.content__photo}>
-           <div className={styles.container}>
-           <div className={styles.tint}>
-            <Image alt="arow" className={styles.img} src="/pic3.png" width={"250"} height={"250"}></Image>
-          </div>
-           </div>
+            <div className={styles.container}>
+              <div className={styles.tint}>
+                <Image
+                  alt="arow"
+                  className={styles.img}
+                  src="/pic3.png"
+                  width={"250"}
+                  height={"250"}
+                ></Image>
+              </div>
+            </div>
             <div className={styles.imgdiv}></div>
           </div>
         </div>
